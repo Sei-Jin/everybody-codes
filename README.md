f122879a139423d4de32c4b416de7884798b70a6
Sei-Jin <seijin.tufts@gmail.com>
2025-03-08 13:16:10 -0400

    Initial project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Everybody Codes
+
+This repository contains my solutions for the [Everybody Codes](https://everybody.codes/) puzzles.
---
+++ src/main/java/year2024/Quest01.java
@@ -0,0 +1,8 @@
+package year2024;
+
+public class Quest01 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
