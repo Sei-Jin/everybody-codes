73de87c7dd18789d8b0c46aecc93dc4e8ad8b256
Sei-Jin <seijin.tufts@gmail.com>
2025-03-08 13:28:16 -0400

    [2024-01-P1] Refactor

--- src/main/java/year2024/Day01.java
+++ src/main/java/year2024/Day01.java
@@ -6,11 +6,22 @@ import java.nio.file.Path;
 
 public class Day01 {
     
-    private static final String filename = "input/day01_p1.txt";
+    private static final String PART_ONE_FILENAME = "input/day01_p1.txt";
     
     public static void main(String[] args) {
-        final var input = readInput();
-        
+        final var partOneInput = parsePartOne();
+        solvePartOne(partOneInput);
+    }
+    
+    private static String parsePartOne() {
+        try {
+            return Files.readString(Path.of(PART_ONE_FILENAME));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private static void solvePartOne(String input) {
         var potions = 0;
         
         for (var i = 0; i < input.length(); i++) {
@@ -24,13 +35,5 @@ public class Day01 {
         }
         
         System.out.println(potions);
-    }
-    
-    private static String readInput() {
-        try {
-            return Files.readString(Path.of(filename));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
